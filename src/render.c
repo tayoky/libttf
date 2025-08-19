fbf7b87886c2d79075d951a0068557137827f7c5
tayoky <tayoky4848@gmail.com>
2025-08-19 11:18:43 +0200

    render : new-grid fitting

--- src/render.c
+++ src/render.c
@@ -5,11 +5,11 @@
 #include "ttf.h"
 #include "internal_ttf.h"
 
-#define convert(funit) (((funit) * glyph->font->font_size + glyph->font->unit_per_em / 2) / glyph->font->unit_per_em)
+#define convert(funit) (((funit) * glyph->font->font_size + glyph->font->unit_per_em - 1) / glyph->font->unit_per_em)
 #define pix(x,y) (((y) * bmp->width) + (x))
 #define ON_CURVE_POINT 0x01
 
-static void check_line(ttf_glyph *glyph,int y,ttf_point *a,ttf_point *b,int *intersections,int *count,int snap){
+static void check_line(ttf_glyph *glyph,int y,ttf_point *a,ttf_point *b,int *intersections,int *count){
 	if(b->y > a->y){
 		ttf_point *c = b;
 		b = a;
@@ -18,17 +18,13 @@ static void check_line(ttf_glyph *glyph,int y,ttf_point *a,ttf_point *b,int *int
  
 	int ay = convert(a->y - glyph->y_min);
 	int by = convert(b->y - glyph->y_min);
-	int ax,bx; 
-	if(snap){
-		ax = convert(a->x - glyph->x_min) * 256;
-		bx = convert(b->x - glyph->x_min) * 256;
-	} else {
+	int ax = convert((a->x - glyph->x_min) * 256);
+	int bx = convert((b->x - glyph->x_min) * 256);
 
-		ax = convert((a->x - glyph->x_min) * 256);
-		bx = convert((b->x - glyph->x_min) * 256);
-	}
+	ay--;
+	by--;
 	//we need one below and on top
-	if(by + 1 > y){
+	if(by >= y){
 		return;
 	}
 	if(ay  < y){
@@ -62,11 +58,16 @@ static void check_curve(ttf_glyph *glyph,int y,ttf_point p[3],int *intersections
 	for(int i=0; i<glyph->font->curves_seg; i++){
 		ttf_point a = bezier(glyph->font->curves_seg,p,i);
 		ttf_point b = bezier(glyph->font->curves_seg,p,i+1);
-		check_line(glyph,y,&a,&b,intersections,count,0);
+		check_line(glyph,y,&a,&b,intersections,count);
 	}
 }
 
 #define pt(index) glyph->pts[first + (((index) - first)%(last - first + 1))]
+
+static void snap(ttf_glyph *glyph,ttf_point *p){
+				p->x = (convert(p->x - glyph->x_min) * glyph->font->unit_per_em + glyph->font->font_size - 1) / glyph->font->font_size + glyph->x_min;
+				p->y = (convert(p->y - glyph->y_min) * glyph->font->unit_per_em + glyph->font->font_size - 1)/ glyph->font->font_size + glyph->y_min;
+}
 
 static int check_intersections(ttf_glyph *glyph,int y,int *intersections){
 	int count = 0;
@@ -91,6 +92,9 @@ static int check_intersections(ttf_glyph *glyph,int y,int *intersections){
 				pts[0].flags |= ON_CURVE_POINT;
 				pts[0].x = (pts[0].x + pts[1].x) / 2;
 				pts[0].y = (pts[0].y + pts[1].y) / 2;
+			} else {
+				//grid finting, we snap the point
+				snap(glyph,&pts[0]);
 			}
 
 			//now we are sure the first point is on curve
@@ -104,10 +108,17 @@ static int check_intersections(ttf_glyph *glyph,int y,int *intersections){
 				pts[2].y = (pts[1].y + pts[2].y) / 2;
 				block_1 = 1;
 
+			} else {
+				//snap the truly on curve point for grid fitting
+				if(pts[1].flags & ON_CURVE_POINT){
+					snap(glyph,&pts[1]);
+				} else {
+					snap(glyph,&pts[2]);
+				}
 			}
 			//we already know first point is on curve
 			if(pts[1].flags & ON_CURVE_POINT){
-				check_line(glyph,y,&pts[0],&pts[1],intersections,&count,1);
+				check_line(glyph,y,&pts[0],&pts[1],intersections,&count);
 			} else {
 				//quadric curve
 				check_curve(glyph,y,pts,intersections,&count);
@@ -146,9 +157,8 @@ ttf_bitmap *ttf_render_glyph(ttf_glyph *glyph){
 		while(count >= 2){
 			int x = intersections[count-2];
 			if(x % 256){
-				uint8_t old = 
-				bmp->bitmap[pix(x/256,y)];
-				bmp->bitmap[pix(x/256,y)] = 255 - ((x % 256) * (255 - old) / 255);
+				uint8_t old = bmp->bitmap[pix(x/256,y)];
+				bmp->bitmap[pix(x/256,y)] += ((255 - (x % 256)) * (255 - old) / 255);
 				x += 256 - (x % 256);
 			}
 
@@ -158,7 +168,8 @@ ttf_bitmap *ttf_render_glyph(ttf_glyph *glyph){
 			}
 
 			if(x < intersections[count-1]){
-				bmp->bitmap[pix(x/256,y)] = intersections[count-1] - x;
+				uint8_t old = bmp->bitmap[pix(x/256,y)];
+				bmp->bitmap[pix(x/256,y)] += ((intersections[count-1] - x) * (255 - old) / 255);
 			}
 			count -= 2;
 		}
